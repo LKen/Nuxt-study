5519113491abecb06f44eaf567f9911a3903242d
KLen <liangchuq@163.com>
2020-12-08 12:49:10 +0800

    eslint test

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -9,6 +9,8 @@
 // import { Context } from '@nuxt/types/app'
 
 export default function ({ store, redirect, route }) {
+  console.log(1)
+
   if (route.fullPath === '/') return
   if (route.fullPath.indexOf('/robot') > -1) return
   if (!store.state.authenticated) {
