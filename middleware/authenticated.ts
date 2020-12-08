24479a3e92a816ff0b3ef9c57862ee7a08af7db2
KLen <liangchuq@163.com>
2020-12-08 15:33:18 +0800

    13213

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -9,7 +9,7 @@
 // import { Context } from '@nuxt/types/app'
 
 export default function ({ store, redirect, route }) {
-  console.log(name2333)
+  console.log(name232333)
 
   if (route.fullPath === '/') return
   if (route.fullPath.indexOf('/robot') > -1) return
